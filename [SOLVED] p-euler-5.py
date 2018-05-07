9e083a8da3880dbc7d29c7e4c5cfe45562b39e6a
rokublak <33192458+rokublak@users.noreply.github.com>
2018-05-08 01:36:07 +1000

    Problem 5

--- [SOLVED] p-euler-5.py
+++ [SOLVED] p-euler-5.py
@@ -16,15 +16,18 @@ logging.basicConfig(level=logging.DEBUG)
 start_time = time.time()
 
 def checker(num):
-    for i in range(11,21): #Don't have to do 1 to 10 since we already know its a mulitple of 2520.
+    # Don't have to do range(1,10) since those numbers are a mulitple of 2520.
+    for i in range(11,21): 
         if num % i == 0:
             continue
         else:
-            return False # Return false if condition is not met
-    return True # Return true if num goes into everything
+            return False 
+    return True 
 
-x = 2520 # 2520 since it has to be a multiple so no point going up by one.
-while checker(x) == False: # while num does not go into every number from 11 to 20, add 2520 until checker is True.
+# 2520 since it has to be a multiple so it's faster to initialize it at 2520.
+x = 2520 
+# while num does not go into every number from 11 to 20, add 2520 until checker is True.
+while checker(x) == False: 
     x += 2520
 
     
