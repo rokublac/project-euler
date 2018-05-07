20bb004570db96536e00d220f8db7d72581a9673
rokublak <33192458+rokublak@users.noreply.github.com>
2018-05-08 01:29:45 +1000

    Problem 3

--- [SOLVED] p-euler-3.py
+++ [SOLVED] p-euler-3.py
@@ -1,15 +1,11 @@
 '''
 
-Project Euler - Problem 3 (Short way)
+Project Euler - Problem 3
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 
 '''
-
-import time
-
-start_time = time.time()
 
 number = 600851475143
 potentialfactor = 2 # If it started as one, while loop will go on forver.
@@ -20,5 +16,3 @@ while (potentialfactor * potentialfactor) < number:
     potentialfactor += 1
     
 print(number)
-
-print('--- %s seconds ---' % (round((time.time() - start_time), 10)))
