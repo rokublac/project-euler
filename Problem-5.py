c2fb7c7bfdee30c0fba2b28bc0bed1fba576a6e1
rokublak <33192458+rokublak@users.noreply.github.com>
2018-05-15 00:08:45 +1000

    Problem 5

--- Problem-5.py
+++ Problem-5.py
@@ -1,5 +1,4 @@
 """
-*** SOLVED ***
 
 PROJECT EULER - PROBLEM 5 - SMALLEST MULTIPLE
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any 
@@ -8,12 +7,6 @@ remainder.
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 """
-import logging
-import time
-
-logging.basicConfig(level=logging.DEBUG)
-
-start_time = time.time()
 
 def checker(num):
     # Don't have to do range(1,10) since those numbers are a mulitple of 2520.
@@ -26,14 +19,13 @@ def checker(num):
 
 # 2520 since it has to be a multiple so it's faster to initialize it at 2520.
 x = 2520 
+
 # while num does not go into every number from 11 to 20, add 2520 until checker is True.
 while checker(x) == False: 
     x += 2520
 
-    
-    
-logging.debug(x)
-logging.debug('--- %s seconds ---' %(time.time() - start_time))
+print(x)
+
 
 
 
