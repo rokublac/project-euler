52545a44a40b5a319892dc7a0aa58d8fefe1f825
rokublak <33192458+rokublak@users.noreply.github.com>
2018-05-15 00:09:44 +1000

    Problem 6

--- Problem-6.py
+++ Problem-6.py
@@ -6,13 +6,7 @@ PROJECT EULER - PROBLEM 6
 https://projecteuler.net/problem=6
 
 """
-import time
-import logging
 from math import sqrt
-
-logging.basicConfig(level=logging.DEBUG)
-
-start_time = time.time()
 
 numList = []
 numList2 = []
@@ -38,7 +32,7 @@ print('Sum of squares from 1 to 100 |', sumOfSquares)
 print('Square of Sum of 1 to 100 |', squareOfSumNumSq)
 print('Difference of Sum of squares and Square of Sum of 1 to 100 |', result)
 
-print('--- %s seconds ---' %(time.time() - start_time))
+
 
     
 
