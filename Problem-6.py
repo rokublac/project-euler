373d96044720f61a1f0dc218d0574643cdfee544
rokublak <33192458+rokublak@users.noreply.github.com>
2018-05-15 00:11:57 +1000

    Problem 6

--- Problem-6.py
+++ Problem-6.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-***SOLVED***
-
 PROJECT EULER - PROBLEM 6
-https://projecteuler.net/problem=6
+The sum of the squares of the first ten natural numbers is,
+
+1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 """
 from math import sqrt
