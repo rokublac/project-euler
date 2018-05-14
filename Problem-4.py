f43492a3187dade93e17cc0a8698b8a81aaa1aea
rokublak <33192458+rokublak@users.noreply.github.com>
2018-05-15 00:07:24 +1000

    Problem 4

--- Problem-4.py
+++ Problem-4.py
@@ -1,6 +1,4 @@
 '''
-***SOLVED***
-
 PROJECT EULER 4 - LARGEST PALINDROME PRODUCT
 
 A palindromic number reads the same both ways. 
@@ -9,14 +7,6 @@ The largest palindrome made from the product of two 2-digit numbers is 9009 = 91
 Find the largest palindrome made from the product of two 3-digit numbers.
 
 '''
-
-
-import logging
-import time
-
-logging.basicConfig(level=logging.DEBUG)
-
-start_time = time.time()
 
 num1 = 900 # highest palindrome will be within the multiples of some number from 900-99
 largestPal = 0 # Set variable to be zero first. Will store largest palindrome
@@ -33,7 +23,5 @@ while num1 < 1000: # Three digit floor condition for num1
           
     num1 += 1
 
-logging.debug(largestPal)
-logging.debug(x) # mulitple number 1 that made the laregst palindrome
-logging.debug(y) # multiple number 2 that made the laregst palindrome
-logging.debug('--- %s seconds ---' %(time.time() - start_time))
+print("The largest palindrome made from a product of two 3-digit numbers is {0} times {1} = {2}.".format(x, y, largestPal))
+
